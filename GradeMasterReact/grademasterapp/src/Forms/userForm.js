fbc6c6b364d063124aa7c4c2feb4f270ac8a65c5
Shamabu <shamabusaleh187@gmail.com>
2024-09-28 23:28:55 +0300

    worked on html

---
+++ GradeMasterReact/grademasterapp/src/Forms/userForm.js
@@ -0,0 +1,13 @@
+import React, { useState  } from 'react';
+import { Button , Form , Container , Row, Col } from 'react-bootstrap'
+
+const UserForm= () =>
+    {
+        return(
+            <h1>
+                Form
+            </h1>
+        );
+
+    };
+
