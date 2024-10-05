ce5797ee1fc6bf88678841bf68d8dba8210a8658
Shamabu <shamabusaleh187@gmail.com>
2024-10-05 21:07:32 +0300

    working on DB

--- GradeMasterAPI/obj/Debug/net8.0/GradeMasterAPI.AssemblyInfo.cs
+++ GradeMasterAPI/obj/Debug/net8.0/GradeMasterAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GradeMasterAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbc6c6b364d063124aa7c4c2feb4f270ac8a65c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("GradeMasterAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GradeMasterAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
